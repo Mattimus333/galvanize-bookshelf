6fcb05daa19f166e2e052a866fa4eea195a39b17
Matt <mmuhr88@gmail.com>
2017-03-07 14:45:16 -0800

    finished the post method

--- routes/books.js
+++ routes/books.js
@@ -7,29 +7,49 @@ const CONFIG = require('../knexfile.js')[ENV];
 const knex = require('knex')(CONFIG);
 
 ROUTER.get('/books', (req, res) => {
-  knex('books').orderBy('title').then((books) => {
+  knex('books')
+  .orderBy('title')
+  .then((books) => {
     res.status(200).json(HUMPS.camelizeKeys(books));
   }).catch((err) => {
     res.status(500);
   });
 });
 
-// ROUTER.post('/books', (req, res) => {
-//   const BOOK = {
-//     title: req.body.title,
-//     author: req.body.author,
-//     genre: req.body.genre,
-//     description: req.body.description,
-//     cover_url: req.body.cover_url,
-//   };
-//   knex('books')
-//   .insert(BOOK)
-//   .then(() => {
-//     res.status(200).json(BOOK);
-//   })
-//   .catch((err) => {
-//     res.status(500);
-//   });
-// });
+ROUTER.get('/books/:id', (req, res) => {
+  knex('books')
+  .where('id', '=', req.params.id)
+  .then((book) => {
+    res.status(200).json(HUMPS.camelizeKeys(book[0]));
+  })
+  .catch((err) => {
+    res.status(500);
+  });
+});
+
+ROUTER.post('/books', (req, res) => {
+  const BOOK = {
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.coverUrl,
+  };
+  knex('books')
+  .insert(BOOK)
+  .then(() => {
+    knex('books')
+    .where('title', '=', BOOK.title)
+    .then((book) => {
+      res.status(200).json(HUMPS.camelizeKeys(book[0]));
+    })
+    .catch((err) => {
+      res.send(401);
+    });
+  })
+  .catch((err) => {
+    res.send(401);
+  });
+});
 
 module.exports = ROUTER;
