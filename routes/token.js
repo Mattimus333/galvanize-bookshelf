f2accdba5e45f66afedd1e640bdecb2f8bd5f407
Matt <mmuhr88@gmail.com>
2017-03-09 19:59:40 -0800

    refactored error handling including bonus part4

--- routes/token.js
+++ routes/token.js
@@ -24,19 +24,26 @@ router.get('/token', (req, res) => {
 });
 
 router.post('/token', (req, res) => {
+  if (req.body.email === undefined) {
+    res.set('Content-Type', 'text/plain');
+    res.status(400).send('Email must not be blank');
+  } else if (req.body.password === undefined) {
+    res.set('Content-Type', 'text/plain');
+    res.status(400).send('Password must not be blank');
+  }
   knex('users')
   .where('email', '=', req.body.email)
   .select('hashed_password', 'id', 'first_name', 'last_name', 'email')
   .then((user) => {
     if (user.length === 0) {
       res.set('Content-Type', 'text/plain');
-      res.send(400, 'Bad email or password');
+      res.status(400).send('Bad email or password');
     }
     bcrypt.compare(req.body.password, user[0].hashed_password)
-    .then( resp => {
-      const claim = {userId: user[0].id};
+    .then((resp) => {
+      const claim = { userId: user[0].id };
       const token = jwt.sign(claim, process.env.JWT_KEY, {
-        expiresIn: '7 days'
+        expiresIn: '7 days',
       });
       delete user[0].hashed_password;
       res.cookie('token', token, {
@@ -44,20 +51,19 @@ router.post('/token', (req, res) => {
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
         secure: router.get('env') === 'production',
       });
-      //going wrong RIGHT HERE
       res.status(200).json(humps.camelizeKeys(user[0]));
     })
-    .catch((err) => {
+    .catch(() => {
       res.set('Content-Type', 'text/plain');
-      res.send(400, 'Bad email or password');
+      res.status(400).send('Bad email or password');
     });
   })
-  .catch((err) => {
-    res.send(400, err);
+  .catch(() => {
+    res.status(400);
   });
 });
 
-router.delete('/token', (req, res) => {
-  
-})
+// router.delete('/token', (req, res) => {
+//
+// })
 module.exports = router;
